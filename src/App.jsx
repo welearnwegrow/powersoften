455af87c718fe13b2d2405c9dc84eefa1b6b1e2f
Jaya Ramchandani <50261431+welearnwegrow@users.noreply.github.com>
2026-06-28 08:52:11 +0100

    Change Router import from BrowserRouter to HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import { Toaster } from "@/components/ui/toaster"
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClientInstance } from '@/lib/query-client'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { HashRouter as Router, ... } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import PageNotFound from './lib/PageNotFound';
 import { AuthProvider, useAuth } from '@/lib/AuthContext';
 import UserNotRegisteredError from '@/components/UserNotRegisteredError';
